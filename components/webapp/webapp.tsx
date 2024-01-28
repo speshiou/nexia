f9e8d974063d201b3c1092797083fab74e357269
pshaung <speshiou@gmail.com>
2024-01-28 21:32:18 +0800

    Fix repeatedly creating executions

--- components/webapp/webapp.tsx
+++ components/webapp/webapp.tsx
@@ -6,12 +6,17 @@ import { AccountProvider } from '../account-provider'
 import ImageCreator from './image_creator'
 import { CreateImageTaskProvider } from '../create-image-task'
 
+// utilize s singleton to prevent the handler from being repeatedly registered 
+const handleMainButtonClick = () => {
+    (document.querySelector("[type=submit]") as HTMLButtonElement).click()
+}
+
 export default function WebApp() {
     const { webApp } = useTelegram()
 
-    webApp?.MainButton?.onClick(() => {
-        (document.querySelector("[type=submit]") as HTMLButtonElement).click()
-    })
+    // the component may be instantiated multiple times
+    webApp?.MainButton?.offClick(handleMainButtonClick)
+    webApp?.MainButton?.onClick(handleMainButtonClick)
 
     return (
         <AccountProvider>
