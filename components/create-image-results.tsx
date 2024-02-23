487326cc480ba20fab8ea9e873977a18da32f72d
pshaung <speshiou@gmail.com>
2024-02-23 14:32:41 +0800

    Tweak the output layout

--- components/create-image-results.tsx
+++ components/create-image-results.tsx
@@ -7,11 +7,11 @@ const CreateImageResults: React.FC = () => {
   const { imageResults, taskState } = useCreateImageTask()
 
   return (
-    <>
+    <div className="flex flex-wrap">
       {...imageResults.map((image) => {
         return <img className="w-1/2 p-1 rounded-md" key={image} src={image} />
       })}
-    </>
+    </div>
   );
 };
 
