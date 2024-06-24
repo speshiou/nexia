e6ba7ea9ebf64f0d853cab7a4a1793f30e7834e4
pshaung <speshiou@gmail.com>
2024-06-24 11:52:25 +0800

    [mini app] fix back button

--- components/widget/scaffold.tsx
+++ components/widget/scaffold.tsx
@@ -62,7 +62,6 @@ export default function Scaffold({
         webApp?.MainButton?.offClick(mainButtonOptions.onClick)
       }
       webApp?.MainButton?.hideProgress()
-      webApp?.BackButton?.offClick(back)
     }
   }, [initialized, mainButtonOptions])
 
