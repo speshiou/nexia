f6d28ea49fe5a81a38cba73ef63e4322ee645322
pshaung <speshiou@gmail.com>
2024-07-11 20:09:49 +0800

    Fix: string.prototype.replaceall called with a non-global regexp argument

--- dictionaries/i18n.ts
+++ dictionaries/i18n.ts
@@ -1,7 +1,10 @@
 export function replaceArgs(text: string, args: { [key: string]: any }) {
   if (args && Object.entries(args).length) {
     Object.entries(args).forEach(([key, value]) => {
-      text = text.replaceAll(new RegExp(`\{\{\s*${key}\s*\}\}`), String(value))
+      text = text.replaceAll(
+        new RegExp(`\{\{\s*${key}\s*\}\}`, 'g'),
+        String(value),
+      )
     })
   }
   return text
